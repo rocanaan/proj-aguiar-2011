f4686c97627de6bcf859cf82b829ee5361f02973
viniciusjserva@gmail.com <viniciusjserva@gmail.com@cedf45fa-8354-0ca4-fa20-ed59a3261951>
2011-06-13 16:47:55 +0000

    Primeiras versões.

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include "..\include\GeradorNumerosAleatorios.h"
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+double inversa(double taxa)
+{
+	return (-1) * (log(1 - GeradorNumerosAleatorios::GetInstance()->Random()) / taxa);
+}
+
+int main(void)
+{
+	//GeradorNumerosAleatorios *gna = GeradorNumerosAleatorios::GetInstance();
+	
+	for (int i=0; i<60; i++)
+		cout << inversa(0.5) << endl;
+		//cout << gna->Random() << endl;
+	
+	return 0;
+}
