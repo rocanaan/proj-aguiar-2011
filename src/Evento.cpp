e52d2f8dda336de759b68fbb4a3547cf71ac874d
rocanaan@gmail.com <rocanaan@gmail.com@cedf45fa-8354-0ca4-fa20-ed59a3261951>
2011-06-15 23:56:54 +0000

    Mudei o nome de uma variável

--- src/Evento.cpp
+++ src/Evento.cpp
@@ -1,25 +1,24 @@
 #include "include\Evento.h"
+#include <iostream>
+#include <cstdlib>
 #include <string>
 
 using namespace std;
 
-    Evento::Evento(EnumTipo ptipo, double ptempo_chegada)
-	{
-        this->tipo = ptipo;
-        this->tempo_chegada = ptempo_chegada;
-    }
+    Evento::Evento(int t, double h){
+                             this->tipo = t;
+                             this->tempo_acontecimento = h;
+                             }
                              
-    EnumTipo Evento::getTipo()
-	{
-        return tipo;
-    }
-	
-    double Evento::getTempoChegada()
-	{
-        return tempo_chegada;
-    }
-	
-    ostream& Evento::imprime(ostream& o)
-	{
-        o << "Evento do tipo " << tipo << " marcado para " << tempo_chegada << endl;
-    }
+          int Evento::getTipo(){
+              return tipo;
+              }
+          double Evento::getTempoAcontecimento(){
+                 return tempo_acontecimento;
+                 }
+          ostream& Evento::imprime(ostream& o){
+               o << "Evento do tipo " << tipo << " marcado para " << hora << endl;
+               }
+                             
+             
+             
