363b117097e7d5ae07cc51b3ee845031d1116bda
FMS2005@gmail.com <FMS2005@gmail.com@cedf45fa-8354-0ca4-fa20-ed59a3261951>
2011-06-18 17:03:11 +0000

    Esse commit vai com a explicação dos outros que eu fiz antes:
    Mudanças na Classe GeradorNumerosAleatorios que passa a ser GeradorTaxaExponencial, e agora incorpora a função inversa também, e possui um metódo para gerar semente inicial a partir do srand.
    
    Além disso criei um bat, de forma que, quem estiver no windows, basta escrever make no cmd que ele faz todo o processo de compilação para você.

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,21 @@
-#include "include\GeradorNumerosAleatorios.h"
 #include "include\Simulador.h"
 #include <iostream>
 #include <math.h>
 
 using namespace std;
 
-double inversa(double taxa)
-{
-	return (-1) * (log(1 - GeradorNumerosAleatorios::GetInstance()->Random()) / taxa);
-}
-
 int main(void)
 {
-	//GeradorNumerosAleatorios *gna = GeradorNumerosAleatorios::GetInstance();
+	/*GeradorTaxaExponencial gna;
+	
+	gna.DefinirSementeAleatoria();
+	
+	for (int i=0; i<60; i++)
+		cout << gna.ExponencialInversa(1) << endl;*/
+		
 	string repeat = "";
 	while (repeat != "N" and repeat !="n")
-    {
-	      /*for (int i=0; i<60; i++)
-		      cout << inversa(0.5) << endl;
-		      //cout << gna->Random() << endl;*/
-		      
+    {     
 	      Simulador s = Simulador(0.3,1);
 	      s.Roda(10);
     
