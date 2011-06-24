aa792f7d9829e6d39fff313202916ccd94683218
FMS2005@gmail.com <FMS2005@gmail.com@cedf45fa-8354-0ca4-fa20-ed59a3261951>
2011-06-24 15:35:48 +0000

    Mudança no GeradorTaxaExponencial para usar o algoritmo de Mersenne, o Gerador foi alterado de forma que seu uso continua exatamente igual, então não há necessidade de mudar nada no resto do código.

--- src/include/GeradorTaxaExponencial.h
+++ src/include/GeradorTaxaExponencial.h
@@ -1,21 +1,20 @@
 //ATENÇÃO:
 //Por estar usando a função srand/rand o valor máximo que pode ser sorteado é RAND_MAX = (2^16) - 1
+#include "Mersenne/randomc.h"
 
-class GeradorTaxaExponencial
+class GeradorTaxaExponencial : CRandomMersenne
 {
 	private:
 		static GeradorTaxaExponencial *instancia;
 	
 	protected:
 		GeradorTaxaExponencial();
-		//GeradorTaxaExponencial(unsigned int semente_inicial);
 		~GeradorTaxaExponencial();	
 	
 	public:
 		static GeradorTaxaExponencial* GetInstancia(); //Retorna a instancia única do gerador de números aleatórios
 	
-		void ResetarGerador(); //Reseta o gerador de números aleatórios
-		void DefinirSemente(unsigned int semente); //Muda a semente do gerador de números aleatórios(resetando ele anteriormente)
+		void DefinirSemente(int semente); //Muda a semente do gerador de números aleatórios(resetando ele anteriormente)
 		double Random(); //Função do Gerador de Números Aleatórios
 		double ExponencialInversa(double taxa); //Faz a inversa do log para encontrar uma exponencial
 };
--- src/make.bat
+++ src/make.bat
@@ -1,7 +1,8 @@
+g++ -c include/mersenne/mersenne.cpp
 g++ -c GeradorTaxaExponencial.cpp
 g++ -c Cliente.cpp
 g++ -c Evento.cpp
 g++ -c Simulador.cpp
-g++ GeradorTaxaExponencial.o Cliente.o Evento.o Simulador.o -o main main.cpp
+g++ mersenne.o GeradorTaxaExponencial.o Cliente.o Evento.o Simulador.o -o main main.cpp
 pause
 
