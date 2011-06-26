8f1a086517c18f26a43efeab5312a659546d6ad3
FMS2005@gmail.com <FMS2005@gmail.com@cedf45fa-8354-0ca4-fa20-ed59a3261951>
2011-06-26 18:18:48 +0000

    Removendo o comentário inutil do arquivo.

--- trunk/src/GeradorTaxaExponencial.cpp
+++ trunk/src/GeradorTaxaExponencial.cpp
@@ -1,6 +1,3 @@
-//ATENÇÃO:
-//Por estar usando a função srand/rand o valor máximo que pode ser sorteado é RAND_MAX = (2^16) - 1
-
 #include "include\GeradorTaxaExponencial.h"
 #include <iostream>
 #include <math.h>
