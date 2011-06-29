7b482f9f0a929600f5b570f5366cee139e42d914
rocanaan@gmail.com <rocanaan@gmail.com@cedf45fa-8354-0ca4-fa20-ed59a3261951>
2011-06-29 01:23:49 +0000

    Modo de 2 chegadas simultaneas por vez implementado.

--- trunk/src/include/Simulador.h
+++ trunk/src/include/Simulador.h
@@ -73,7 +73,7 @@ class Simulador{
     public:
 		Simulador(double ptaxa_chegada, double ptaxa_servico, bool deterministico);
 		~Simulador();	
-		void Roda(int num_total_clientes, int rodada_atual, bool debug_eventos, bool deterministico, bool determina_transiente);
+		void Roda(int num_total_clientes, int rodada_atual, bool debug_eventos, bool deterministico, bool determina_transiente, bool dois_por_vez);
 		void CalculaResultados(int n, int servidos1, double t, int rodada, bool debug_eventos);
 		void LimpaResultadosParciais();
 		void GeraDadosGrafico(int rodada, double pN1, double pN2, double pNq1, double pNq2, double pW1, double pW2, double pT1, double pT2, double pV_W1, double pV_W2);
