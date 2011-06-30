5212b187c6c2792f44cb021858da9f63b234d92a
FMS2005@gmail.com <FMS2005@gmail.com@cedf45fa-8354-0ca4-fa20-ed59a3261951>
2011-06-30 00:13:15 +0000

    Separado a classe Config da main.cpp.
    Editado o bat para compilar certinho.

--- src/make.bat
+++ src/make.bat
@@ -3,6 +3,7 @@ g++ -c GeradorTaxaExponencial.cpp
 g++ -c Cliente.cpp
 g++ -c Evento.cpp
 g++ -c Simulador.cpp
+g++ -c include/Config.h
 g++ mersenne.o GeradorTaxaExponencial.o Cliente.o Evento.o Simulador.o -o main main.cpp
 pause
 
